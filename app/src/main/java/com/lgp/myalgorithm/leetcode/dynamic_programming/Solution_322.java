6fb4553fdbb7d48b72993d6ad92b2cca2f6487ae
lgp <luoguopeng@xa.com>
2020-09-03 17:07:10 +0800

    提交测试

---
+++ app/src/main/java/com/lgp/myalgorithm/leetcode/dynamic_programming/Solution_322.java
@@ -0,0 +1,16 @@
+package com.lgp.myalgorithm.leetcode.dynamic_programming;
+
+/**
+ * 创建者     罗国鹏
+ * 创建时间   2020/9/3 16:57
+ * 描述       给定不同面额的硬币 coins 和一个总金额 amount。编写一个函数来计算可以凑成总金额所需的最少的硬币个数。如果没有任何一种硬币组合能组成总金额，返回 -1。
+ * 更新者     $
+ * 更新时间   $
+ * 更新描述
+ */
+public class Solution_322 {
+    public int coinChange(int[] coins, int amount) {
+
+        return 0;
+    }
+}
